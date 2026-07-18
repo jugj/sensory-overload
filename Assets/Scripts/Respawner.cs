80e33bb8f5bd34326dfd47684984cebaef110bd9
Leo <jugj@users.noreply.github.com>
2026-07-18 11:00:22 +0200

    Änderungen von Leo am 2026-07-18 11:00:22

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -10,14 +10,28 @@ public class Respawner : MonoBehaviour
     [SerializeField]
     float x_spawn_pos = -8f;
 
+
+    [SerializeField]
+    GameObject player_active;
+    [SerializeField]
+    GameObject player;
+
+    void SpawnPlayer(){
+        Vector2 spawnpoint = new Vector2(x_spawn_pos, y_spawn_pos);
+        player_active = Instantiate(player, spawnpoint, Quaternion.identity);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        SpawnPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(player_active == null){
+            SpawnPlayer();
+        }
     }
 }
